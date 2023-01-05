fccd3f72bba64f2934b19f4d751290a637d39675
hakuna00723 <110534352+hakuna00723@users.noreply.github.com>
2023-01-05 22:39:28 +0000

    Deploying to gh-pages from @ hakuna00723/goit-js-hw-09@deed87fc0493fed3804a6515f441d1b2a8ec7c63 🚀

---
+++ 01-color-switcher.8ac5c157.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.8ac5c157.js.map
