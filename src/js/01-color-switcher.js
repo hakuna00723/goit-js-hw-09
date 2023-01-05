ae43ba77fbcb8bbbbe3d5bdc06264bacba92bb74
Serhii <110534352+hakuna00723@users.noreply.github.com>
2023-01-05 23:25:46 +0100

    01-color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const bckgr = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bckgr.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
